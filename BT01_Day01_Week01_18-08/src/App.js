e1e1fa5132a520b972def8f435231e9187153897
KieuLinh0701 <uynukieulinh@gmail.com>
2025-09-08 10:40:29 +0700

    update

---
+++ BT01_Day01_Week01_18-08/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Uý Nữ Kiều Linh</h1>
+      <p>Sinh viên ngành Công nghệ Thông tin</p>
+      <p>Mã số sinh viên: 21110896</p>
+      <p>Trường Đại Học Sư phạm Kỹ thuật Thành phố Hồ Chí Minh</p>
+    </div>
+  );
+}
+
+export default App;
